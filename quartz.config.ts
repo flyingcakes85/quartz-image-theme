0f1f274ad90a2e6d6117801348b888d4d30a3b71
Snehit Sah <snehitsah@protonmail.com>
2023-12-26 17:27:59 +0530

    ci: deploy
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quartz-image-theme.snehit.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
